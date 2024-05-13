4eabe6ac6cdb5faf83f4fd46e5ead1bc75d78ac7
SASADA <yusuke_sasada@r.recruit.co.jp>
2024-05-13 12:41:20 +0900

    最初のコミット

---
+++ HelloSpringBoot/src/main/java/com/techacademy/HelloSpringBootController.java
@@ -0,0 +1,30 @@
+package com.techacademy;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloSpringBootController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello SpringBootの世界へ!";
+    }
+    
+    @GetMapping("now")
+    public String dispTime() {
+        var now = LocalDateTime.now().format( DateTimeFormatter.ofPattern("HH:mm:ss") );
+
+        return "現在時刻：" + now;
+    }
+    @GetMapping("/plus/{val1}/{val2}")
+    public String calcPlus(@PathVariable int val1, @PathVariable int val2) {
+        var res = 0;
+        res = val1 + val2;
+        return "計算結果：" + res;
+    }
+}
